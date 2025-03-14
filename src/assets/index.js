4edefe3c24be87d3c47d9c9bd3940d27dbcff7fe
unknown <ibraheemfatiuoyebayo@gmail.com>
2025-03-14 15:41:18 +0100

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,22 @@
+import bg from './bg.jpg'
+
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+
+import confetti from './confetti.gif'
+
+import CakeSVG from './svg/CakeSVG'
+
+
+
+export {
+    bg,
+    img1,
+    img2,
+    img3,
+    img4,
+    confetti,
+    CakeSVG
+}
